a38c2913e07a308b86898d0faebb0944e936582a
Avirup Dutta <39208694+avirupdutta@users.noreply.github.com>
2020-06-21 13:05:59 +0530

    minor update in keys.js

--- config/keys.js
+++ config/keys.js
@@ -2,6 +2,6 @@ module.exports = {
 	mongoURI: `mongodb+srv://${process.env.MONGO_USERNAME}:${
 		process.env.MONGO_PASSWORD
 	}@cluster0-pxge0.mongodb.net/${
-		process.env.MONO_DB_NAME
+		process.env.MONGO_DB_NAME
 	}?retryWrites=true&w=majority`
 };
